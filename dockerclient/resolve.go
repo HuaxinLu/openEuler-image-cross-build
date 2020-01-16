70597557e70e256e03eabd769a9f6df0f3ff94e5
HuaxinLu <1539327763@qq.com>
2020-01-16 10:34:12 +0800

    change some func

--- dockerclient/resolve.go
+++ dockerclient/resolve.go
@@ -17,6 +17,16 @@ type EventPull struct {
     } `json:"progressDetail"`
 }
 
+type EventPush struct {
+    Status         string `json:"status"`
+    Error          string `json:"error"`
+    Progress       string `json:"progress"`
+    ProgressDetail struct {
+        Current int `json:"current"`
+        Total   int `json:"total"`
+    } `json:"progressDetail"`
+}
+
 func ResolvePull(resp io.ReadCloser, image string) error {
 	var event *EventPull
 	d := json.NewDecoder(resp)
@@ -41,3 +51,31 @@ func ResolvePull(resp io.ReadCloser, image string) error {
     }
     return nil
 }
+
+func ResolvePush(resp io.ReadCloser, image string) error {
+    var event *EventPush
+    d := json.NewDecoder(resp)
+    for {
+        if err := d.Decode(&event); err != nil {
+            if err == io.EOF {
+                break
+            }
+            panic(err)
+        }
+        if event.Error != "" {
+            fmt.Printf("Error:  %s\n", event.Error)
+        }
+    }
+	/*
+    if event != nil {
+        if strings.Contains(event.Status, "Downloaded newer image") {
+            fmt.Printf("Downloaded newer image for %s\n", image)
+        }
+        if strings.Contains(event.Status, "Image is up to date") {
+            fmt.Printf("Image is up to date for %s\n", image)
+        }
+    }
+	*/
+    return nil
+}
+
--- push/push.go
+++ push/push.go
@@ -1,68 +1,53 @@
 package push
 
 import(
-    "fmt"
-    //"os"
-    "io"
-	//"strings"
-    "context"
-    "encoding/json"
-    "encoding/base64"
-    "github.com/docker/docker/client"
-    "github.com/docker/docker/api/types"
+	"fmt"
+	"log"
+	"context"
+	"encoding/json"
+	"encoding/base64"
+	"github.com/docker/docker/api/types"
+	docker "hwbuild/dockerclient"
 )
 
 //push镜像
 func PushImage(image string) error {
-    cli, err := client.NewClient("tcp://0.0.0.0:8088", "v1.40", nil, nil)
-    if err != nil {
-        fmt.Printf("%v\n", err)
-        return err
-    }
-
-	auth := types.AuthConfig{Username: "huaxinlu", Password: "l95h08x27",}
-	encodedJSON, err := json.Marshal(auth)
-		
-    if err != nil {
-        fmt.Printf("%v\n", err)
-        return err
+	// create docker client
+	cli, err := docker.CreateClient()
+	if err != nil {
+		log.Fatal(err, " :unable to create docker client.")
+		return err
 	}
-	
+
+	// get username
+	fmt.Print("Username: ")
+	var userName string
+	fmt.Scanln(&userName)
+	// get password
+	fmt.Print("Password: ")
+	var password string
+	fmt.Scanln(&password)
+
+	// change string to json
+	auth := types.AuthConfig{Username: userName, Password: password,}
+	encodedJSON, err := json.Marshal(auth)
+	if err != nil {
+		log.Fatal(err, " :unable to encode auth.")
+		return err
+	}
 	authStr := base64.URLEncoding.EncodeToString(encodedJSON)
+	
+	//push
+	fmt.Printf("Start to push...\n")
 	pushResp, err := cli.ImagePush(context.Background(),
 		image,
 		types.ImagePushOptions{RegistryAuth: authStr,})
-	//io.Copy(os.Stdout, pushResp)
+	if err != nil {
+		log.Fatal(err, " :unable to push docker image.")
+		return err
+	}
 	
-    d := json.NewDecoder(pushResp)                                                     
-    
-    type Event struct {
-        Status         string `json:"status"`                                          
-        Error          string `json:"error"`
-        Progress       string `json:"progress"`                                        
-        ProgressDetail struct {
-            Current int `json:"current"`                                               
-            Total   int `json:"total"`                                                 
-        } `json:"progressDetail"`                                                      
-    }                                                                                  
-    
-    var event *Event
-    fmt.Printf("Start to push...\n")                                                   
-    for {
-        if err := d.Decode(&event); err != nil {                                       
-            if err == io.EOF {                                                         
-                break                                                                  
-            }
-            panic(err)                                                                 
-        }                                                                              
-        
-        if event.Error != "" {  
-            fmt.Printf("Error: %s\n", event.Error)
-        }
-		//fmt.Println(event.Status)
-    }
-
+	err = docker.ResolvePush(pushResp, image)
 	fmt.Printf("Push finished...\n")
-
 	return err
 }
