0b0462becdf4f84705e9f6c61a33171b32c34773
HuaxinLu <1539327763@qq.com>
2020-01-16 10:03:38 +0800

    change format

--- pull/pull.go
+++ pull/pull.go
@@ -12,12 +12,12 @@ import(
 
 // pull image
 func PullImage(image string) error { 
-    // create docker client
+	// create docker client
 	cli, err := docker.CreateClient()
 	if err != nil {
-        log.Fatal(err, " :unable to create docker client.")
-        return err
-    }
+		log.Fatal(err, " :unable to create docker client.")
+		return err
+	}
 
 	// get username
 	fmt.Print("Username: ")
@@ -25,29 +25,29 @@ func PullImage(image string) error {
 	fmt.Scanln(&userName)
 	// get password
 	fmt.Print("Password: ")
-    var password string
-    fmt.Scanln(&password)
+	var password string
+	fmt.Scanln(&password)
 
 	// change string to json
-    auth := types.AuthConfig{Username: userName, Password: password,}
-    encodedJSON, err := json.Marshal(auth)
-    if err != nil {
-        log.Fatal(err, " :unable to encode auth.")
-        return err
-    }
-    authStr := base64.URLEncoding.EncodeToString(encodedJSON)
+	auth := types.AuthConfig{Username: userName, Password: password,}
+	encodedJSON, err := json.Marshal(auth)
+	if err != nil {
+		log.Fatal(err, " :unable to encode auth.")
+		return err
+	}
+	authStr := base64.URLEncoding.EncodeToString(encodedJSON)
 
 	// pull
 	fmt.Printf("Start to pull...\n")
-    pullResp, err := cli.ImagePull(context.Background(), 
+	pullResp, err := cli.ImagePull(context.Background(), 
 		image,
-        types.ImagePullOptions{RegistryAuth: authStr,})
+		types.ImagePullOptions{RegistryAuth: authStr,})
 	if err != nil {
 		log.Fatal(err, " :unable to pull docker image")
-        return err
+		return err
 	}
 	
-    err = docker.ResolvePull(pullResp, image)
-    fmt.Printf("Pull finished...\n")
+	err = docker.ResolvePull(pullResp, image)
+	fmt.Printf("Pull finished...\n")
 	return err
 }
